af509e7a395f586f092b3deffe945a4a6b08d050
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-26 02:57:00 +0530

    Initial Project Setup

---
+++ CalculatorSwiftUI/CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Mohit Gupta on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
